e61a1c44be1cab75df79c3e185588f71db2a7f26
jessamynsmith <geekchick77@gmail.com>
2023-01-09 08:41:00 -0700

    Initial Commit

---
+++ Underquoted/UnderquotedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderquotedApp.swift
+//  Underquoted
+//
+//  Created by Jessamyn Smith on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct UnderquotedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
